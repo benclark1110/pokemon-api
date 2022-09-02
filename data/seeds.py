8144658e5df4d80aba2ef969cc488503df504d9a
benclark1110 <benclark1110@gmail.com>
2022-09-01 20:25:14 -0400

    adding sample data

---
+++ data/seeds.py
@@ -0,0 +1,14 @@
+data = [
+    {
+        "name": "pikachu",
+        "type": "electric",
+        "caught": True,
+        "id": 1
+    },
+    {
+        "name": "squirtle",
+        "type": "water",
+        "caught": False,
+        "id": 2
+    }
+]
