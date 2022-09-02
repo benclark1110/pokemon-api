fab16977925c0a78f66404e04e247d384e14164f
benclark1110 <benclark1110@gmail.com>
2022-09-02 19:12:12 -0400

    cleanup and adding pokemon class

--- app.py
+++ app.py
@@ -4,12 +4,8 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 def get_database():
-    # from pymongo import MongoClient
-    # import pymongo
-
     CONNECTION_STRING = "mongodb+srv://benclark1110:pokemonapi@cluster0.z17f56o.mongodb.net/?retryWrites=true&w=majority"
     
-    # from pymongo import MongoClient
     client = MongoClient(CONNECTION_STRING)
 
     return client['pokemonDatabase']
@@ -25,15 +21,6 @@ charmnder_data = {
     "caught": True
 }
 
-# class Pokemon:
-#     _id: int
-#     snamelug: str
-#     type: str
-#     caught: bool
-
-#     def to_json(self):
-#         return jsonify(self, exclude_none=True)
-
 class Pokemon():
 
     def __init__(self, _id, name, type, caught):
@@ -41,7 +28,6 @@ class Pokemon():
         self.name = name
         self.type = type
         self.caught = caught
-
 
 @app.route("/")
 def get_all_pokemon():
@@ -55,15 +41,10 @@ def find_one(_id):
 
 @app.route("/add_pokemon", methods=["PUT", "POST"])
 def add_pokemon():
-    print(request.get_json())
-
     raw_pokemon = request.get_json()
-    print(raw_pokemon["name"])
-    # pokemon = Pokemon(raw_pokemon["_id"], raw_pokemon["name"], raw_pokemon["type"], raw_pokemon["caught"])
-    # collection_name.insert_one(pokemon)
+    pokemon = Pokemon(raw_pokemon["_id"], raw_pokemon["name"], raw_pokemon["type"], raw_pokemon["caught"])
+    collection_name.insert_one(pokemon.__dict__)
     return 'added'
-
-
 
 @app.errorhandler(404)
 def resource_not_found(e):
--- data/testing.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-
-myclient = pymongo.MongoClient("mongodb://localhost:8080/")
-mydb = myclient["mydatabase"]
-
-mycol = mydb["customers"]
-
-print(mydb.list_collection_names())
